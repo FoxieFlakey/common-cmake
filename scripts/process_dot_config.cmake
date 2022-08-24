922bc1afdbe340b2d887dd79408c95052c0518af
Fluffy Fox <cosmo8072@gmail.com>
2022-08-24 18:16:17 +0700

    init

---
+++ scripts/process_dot_config.cmake
@@ -0,0 +1,23 @@
+
+# Process .config files
+# Create header
+file(READ ${PROJECT_SOURCE_DIR}/.config filedata)
+string(REGEX REPLACE "#" "//" filedata "${filedata}")
+string(REGEX REPLACE "CONFIG" "#define CONFIG" filedata "${filedata}")
+string(REGEX REPLACE "(#define[^=]+)=y\n" "\\1 1\n" filedata "${filedata}")
+string(REGEX REPLACE "(#define[^=]+)=n\n" "\\1 0\n" filedata "${filedata}")
+string(REGEX REPLACE "(#define[^=]+)=([^\n]+)\n" "\\1 \\2\n" filedata "${filedata}")
+file(WRITE ${PROJECT_BINARY_DIR}/src/kconfig.h "${filedata}")
+
+# Create cmake config
+file(READ ${PROJECT_SOURCE_DIR}/.config filedata)
+string(REGEX REPLACE "CONFIG" "set(CONFIG" filedata "${filedata}")
+string(REGEX REPLACE "(set[^=]+)=y\n" "\\1, ON)\n" filedata "${filedata}")
+string(REGEX REPLACE "(set[^=]+)=n\n" "\\1, OFF)\n" filedata "${filedata}")
+string(REGEX REPLACE "(set[^=]+)=([^\n]+)\n" "\\1 \\2)\n" filedata "${filedata}")
+string(REGEX REPLACE "(CONFIG_[A-Z0-9a-z_]+)," "\\1" filedata "${filedata}")
+file(WRITE ${PROJECT_BINARY_DIR}/config.cmake "${filedata}")
+
+# Load CMake config
+include(${PROJECT_BINARY_DIR}/config.cmake)
+
