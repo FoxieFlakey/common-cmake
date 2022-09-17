53f9396e617cf703bce8ffd139014b7ac5c15f55
Fox <67861857+FluffyFoxUwU@users.noreply.github.com>
2022-09-17 22:40:57 +0800

    Update compiler_and_link_flags.cmake

--- scripts/compiler_and_link_flags.cmake
+++ scripts/compiler_and_link_flags.cmake
@@ -56,7 +56,7 @@ if (DEFINED CONFIG_TSAN)
   target_link_options(ProjectCommonFlags INTERFACE "-fsanitize=thread")
 endif()
 
-if (DEFINED CONFIG_XRAY)
+if (DEFINED CONFIG_LLVM_XRAY)
   target_compile_options(ProjectCommonFlags INTERFACE "-fxray-instrument")
   target_compile_options(ProjectCommonFlags INTERFACE "-fxray-instruction-threshold=1")
   target_link_options(ProjectCommonFlags INTERFACE "-fxray-instrument")
